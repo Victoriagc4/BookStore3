4c06ddefc4c3952a9418a35d94d45bbb41588ace
Marcos <marcosjfr3@gmail.com>
2024-06-22 16:54:10 -0400

    user added

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
