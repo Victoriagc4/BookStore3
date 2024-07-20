96003acc515d0f2e372c11a57a52ecc8143358c0
Victoria González <victoriagc4@gmail.com>
2024-07-20 17:59:35 -0400

    book admin

---
+++ books/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price') 
+    list_filter = ('author',)
+    search_fields = ('title', 'author')
+    ordering = ('title', 'price')
+    
+admin.site.register(Book, BookAdmin)
