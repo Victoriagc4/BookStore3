bef76e877f2dbcf4811c74934e166bf2b63b437e
Victoria González <victoriagc4@gmail.com>
2024-08-10 18:13:16 -0400

    reviews

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Book(models.Model):
@@ -16,3 +17,11 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+    
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    review = models.CharField(max_length=300)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.review
